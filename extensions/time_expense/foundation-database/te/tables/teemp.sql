1771b74edaa98f070e216b946519902bd0b3f9d6
Scott Zuke <szuke@coreservicesgroup.com>
2015-11-06 23:53:23 -0500

    RESOLVED

--- extensions/time_expense/foundation-database/te/tables/teemp.sql
+++ extensions/time_expense/foundation-database/te/tables/teemp.sql
@@ -5,5 +5,7 @@ select xt.add_column('teemp','teemp_id', 'serial', 'not null', 'te');
 select xt.add_column('teemp','teemp_emp_id', 'integer', '', 'te');
 select xt.add_column('teemp','teemp_contractor', 'boolean', 'default false', 'te');
 select xt.add_constraint('teemp', 'teemp_teemp_emp_id_fkey', 'foreign key (teemp_emp_id) references emp (emp_id)', 'te');
+select xt.add_constraint('teemp', 'teemp_pkey', 'primary key (teemp_id)', 'te');
+select xt.add_constraint('teemp', 'unq_teemp_emp_id', 'UNIQUE(teemp_emp_id)', 'te');
 
 comment on table te.teemp is 'Time Expense Employee';
